9b4f5881e00b0c27b5aed88d6eac9095dad2a064
pppi5367 <111578053+pppi5367@users.noreply.github.com>
2025-11-21 22:31:03 +0800

    update character info

--- Character.h
+++ Character.h
@@ -14,5 +14,9 @@ class Character {
 
 // other characters may be like:
 class Elliot : public Character {};
+class BigJay : public Character {};
+class YaJu : public Character {};
+class OldJuang : public Character {};
+class MingJen : public Character {};
 
 #endif
