a65772d554896f880df4be2f48374d4d699da300
riley0818 <rileyhsu818@gmail.com>
2025-11-26 21:34:12 +0800

    create .clang-format

--- Player.cpp
+++ Player.cpp
@@ -1,8 +1,11 @@
 #include "Player.h"
 
-void Player::skillInvalid() {
+char Player::chooseDirection() {}
 
+void Player::takeDamage(int dmg) {}
+
+bool Player::isDead() {
+	return (hp == 0) ? true : false; // if hp = 0, the player's dead
 }
-void Player::getExtraBlood() {
-    
-}
+
+void Player::applySkillTrigger() {}
